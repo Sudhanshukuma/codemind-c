70413e1fd47ecbe42ab75fbb7bc699496b78bd2c
Sudhanshukuma <97438302+Sudhanshukuma@users.noreply.github.com>
2022-05-20 11:08:59 +0530

    #include<stdio.h> int main() {     int n,p,m;     scanf("%d%d",&n,&p);     m=(n>p)?n:p;     while(1)     {         if(m%n==0&&m%p==0)         {             printf("%d",m);             break;         }         ++m;     }     return 0; }

--- LCM.c
+++ LCM.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,i;
-    scanf("%d%d",&a,&b);
-    i=a>b?a:b;
+    int n,p,m;
+    scanf("%d%d",&n,&p);
+    m=(n>p)?n:p;
     while(1)
-    {if(i%a==0&&i%b==0)
-    {printf("%d",i);break;}
-    i++;}
+    {
+        if(m%n==0&&m%p==0)
+        {
+            printf("%d",m);
+            break;
+        }
+        ++m;
+    }
     return 0;
 }
